dfd32cd22d4081b6fb0ba835b6c5daea572de22e
jaime ambriz <jaime.ambriz@yahoo.com>
2017-10-29 21:58:02 -0600

    added the giveItem and addMods function

--- game.js
+++ game.js
@@ -1,16 +1,19 @@
 var health = 100
 var hits = 0
 var targetName = "Sagat"
-var input = ""
+// var input = ""
 var compHealth = 100
 var compHits = 0
 function slap(input) {
-    if (input == "superKick") {
-        compHealth -= 20
-    } else if (input == "kick") {
-        compHealth -= 10
-    } else {
-        compHealth--
+   var victoryStatus = "You win"
+    if (input == "superKick" && compHealth > 0) {
+        compHealth -= 20 * addMods(ryu)
+    } else if (input == "kick" && compHealth > 0) {
+        compHealth -= 10 * addMods(ryu)
+    } else if (input == "punch" && compHealth > 0){
+        compHealth -= 1 * addMods(ryu)
+    }else {
+        return victoryStatus
     }
     compHits++
     update()
@@ -18,6 +21,7 @@ function slap(input) {
 update()
 
 function update() {
+    // document.getElementById("vitory-status").innerText =victoryStatus
     document.getElementById("health").innerText = health
     document.getElementById("hits").innerText = hits
     // document.getElementById("victoryStatus").innerHTML = name
@@ -30,12 +34,12 @@ function update() {
 var sagat = new Player("Sagat", 140, )
 var ryu = new Player("Ryu", 140, )
 var items = {
-    hadouken: new Item("Hadouken", 50, "Blast of energy"),
-    tatsumaki: new Item("Tatsumaki", 40, "Super powerfull spinningkick"),
-    shoryuken: new Item("Shoryouken", 35, "Awesome upper cut"),
-    tigerGenocide: new Item("Tiger Genocide", 50 ,"7 hit combo"),
-    tigerUppercut: new Item("Tiger Uppercut", 40, "Super strong uppercut"),
-    tigerShot: new Item("Tiger Shot", 35,"Fire Blast"),
+    hadouken: new Item("Hadouken", 2.2, "Blast of energy"),
+    tatsumaki: new Item("Tatsumaki", 1.3, "Super powerfull spinningkick"),
+    shoryuken: new Item("Shoryouken", 1.7, "Awesome upper cut"),
+    tigerGenocide: new Item("Tiger Genocide", 2.3, "7 hit combo"),
+    tigerUppercut: new Item("Tiger Uppercut", 1.5, "Super strong uppercut"),
+    tigerShot: new Item("Tiger Shot", 1.2, "Fire Blast")
 
 }
 function Player(name, health, ) {
@@ -50,11 +54,39 @@ function Player(name, health, ) {
 function Item(name, damage, description) {
     this.name = name;
     this.damage = damage;
-    this.description = description;
+    this.description = description
 }
-function giveSpecialMove() {
-    ryu.items.push(items.hadouken, items.shoryuken, items.tatsumaki)
-    sagat.items.push(items.tigerUppercut, items.tigerShot, items.tigerGenocide)
 
+// function giveSpecialMove() {
+//     ryu.items.push(items.hadouken, items.shoryuken, items.tatsumaki)
+//     sagat.items.push(items.tigerUppercut, items.tigerShot, items.tigerGenocide)
+
+// }
+// giveSpecialMove()
+
+function addMods(player) {
+    var totalDamage = 1
+    for (var i = 0; i < player.items.length; i++) {
+        var damage = player.items[i].damage;
+        if (damage > 0) {
+            totalDamage = damage
+        }
+    }
+    console.log(damage)
+    return totalDamage
 }
-giveSpecialMove()
+addMods(ryu)
+
+function giveItem(item){
+ var out = ""
+    if(item == "hadouken"){
+        out = ryu.items.push(items.hadouken)
+        console.log(out)
+    }else if(item == "tatsumaki"){
+        out = ryu.items.push(items.tatsumaki)
+    }else {
+        out = ryu.items.push(items.shoryuken)
+    }
+    return out
+}
+
